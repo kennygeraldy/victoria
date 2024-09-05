e085c153e9e69108239fec813b9f3656b0efab85
Kenny Geraldy Chandra <kennygeraldy@gmail.com>
2024-09-05 10:48:18 +0700

    add rumus hitung luas lingkaran

--- pre-test/day-1/HelloWorld/src/Main.java
+++ pre-test/day-1/HelloWorld/src/Main.java
@@ -1,39 +1,56 @@
+import java.util.Scanner;
+
 public class Main {
+
+    Scanner sc = new Scanner(System.in);
 
     public Main() {
 
-        int umur;
-        umur = 22;
+//        int umur;
+//        umur = 22;
+//
+//        String myName;
+//        myName = "Kenny Geraldy Chandra";
+//
+//        long gaji;
+//        gaji = 0;
+//
+//        double duit;
+//        duit = 1000000000;
+//
+//        float tinggi;
+//        tinggi = 180.50f;
+//
+//        char grade;
+//        grade = 'A';
+//
+//        boolean isJavaFun;
+//        isJavaFun = true;
+//
+//
+//        System.out.println("Hello World\n");
+//        System.out.println("Biodata Saya");
+//        System.out.println("===========================");
+//        System.out.println("Nama saya adalah "+myName);
+//        System.out.println("Umur saya adalah "+umur);
+//        System.out.println("Tinggi saya adalah "+tinggi);
+//        System.out.println("Duit saya adalah Rp. "+duit);
+//        System.out.println("Gaji saya adalah Rp. "+gaji);
+//        System.out.println("Nilai rata-rata saya adalah "+grade);
+//        System.out.println("Menurut saya java itu mengasikan "+isJavaFun);
 
-        String myName;
-        myName = "Kenny Geraldy Chandra";
+//        Latihan ke 2
 
-        long gaji;
-        gaji = 0;
+        final float pi = 3.14F;
+        int radius;
 
-        double duit;
-        duit = 1000000000;
-
-        float tinggi;
-        tinggi = 180.50f;
-
-        char grade;
-        grade = 'A';
-
-        boolean isJavaFun;
-        isJavaFun = true;
-
-
-        System.out.println("Hello World\n");
-        System.out.println("Biodata Saya");
-        System.out.println("===========================");
-        System.out.println("Nama saya adalah "+myName);
-        System.out.println("Umur saya adalah "+umur);
-        System.out.println("Tinggi saya adalah "+tinggi);
-        System.out.println("Duit saya adalah Rp. "+duit);
-        System.out.println("Gaji saya adalah Rp. "+gaji);
-        System.out.println("Nilai rata-rata saya adalah "+grade);
-        System.out.println("Menurut saya java itu mengasikan "+isJavaFun);
+        do {
+            System.out.print("Masukkan Luas: ");
+            radius = sc.nextInt();
+            sc.nextLine();
+        } while (radius == 0); {
+            System.out.print(pi * (radius * radius));
+        }
 
     }
     public static void main(String[] args) {
