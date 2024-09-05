f458f4ec9931c19cd797a583204053e9434cfe1d
Kgeraldy <kennygeraldy@gmail.com>
2024-09-05 10:03:53 +0700

    add hello world

---
+++ pre-test/day-1/HelloWorld/main.java
@@ -0,0 +1,11 @@
+public class main {
+
+    public main() {
+
+        System.out.println("Hello World");
+
+        public static void main(String[] args) {
+            new main();
+
+        }
+    }
