384b7f754022e38bf529167b8f5fe7457c5a4e34
Kenny Geraldy Chandra <kennygeraldy@gmail.com>
2024-09-06 13:45:43 +0700

    add get Data By Id

--- exercise/day-2/section-1/demo/first-application/src/main/java/com/example/first_application/FirstApplication.java
+++ exercise/day-2/section-1/demo/first-application/src/main/java/com/example/first_application/FirstApplication.java
@@ -4,6 +4,7 @@ import com.example.first_application.response.GetAssetResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.reflect.Array;
@@ -24,6 +25,9 @@ public class FirstApplication {
 		ArrayList<GetAssetResponse> List = new ArrayList<>();
 		var response = GetAssetResponse.builder().id(1L).fullName("Kenny Geraldy Chandra").umur(22).gender("Laki-Laki").address("Jl. Kintamani No.2").build();
 		var response1 = GetAssetResponse.builder().id(2L).fullName("John Doe").umur(23).gender("Laki-Laki").address("Jl. Kintamani No.3").build();
+		var response2 = GetAssetResponse.builder().id(3L).fullName("James Bond").umur(26).gender("Laki-Laki").address("Jl. Kintamani No.5").build();
+		var response3 = GetAssetResponse.builder().id(4L).fullName("Kapti").umur(13).gender("Laki-Laki").address("Jl. Kintamani No.6").build();
+		var response4 = GetAssetResponse.builder().id(5L).fullName("Kurniawan").umur(21).gender("Laki-Laki").address("Jl. Kintamani No.7").build();
 
 		response.setFullName("KENNY G");
 
@@ -43,6 +47,26 @@ public class FirstApplication {
 //		return response;
 	}
 
+	@GetMapping("/assets/{id}")
+	public GetAssetResponse getAssetById(
+			@PathVariable Long id
+	){
+
+		List<GetAssetResponse> asset = new ArrayList<>();
+		asset.add(GetAssetResponse.builder().id(1L).fullName("Kenny Geraldy Chandra").umur(22).gender("Laki-Laki").address("Jl. Kintamani No.2").build());
+		asset.add(GetAssetResponse.builder().id(2L).fullName("Chandra").umur(22).gender("Laki-Laki").address("Jl. Kintamani No.2").build());
+		asset.add(GetAssetResponse.builder().id(3L).fullName("James Bond").umur(22).gender("Laki-Laki").address("Jl. Kintamani No.2").build());
+		asset.add(GetAssetResponse.builder().id(4L).fullName("Cherry Jiam").umur(22).gender("Perempuan").address("Jl. Kintamani No.2").build());
+		asset.add(GetAssetResponse.builder().id(5L).fullName("Jomos").umur(22).gender("Laki-Laki").address("Jl. Kintamani No.2").build());
+//		return GetAssetResponse.builder().id(id).fullName(fullName).build();
+
+		for (GetAssetResponse getAssetResponse : asset){
+			if(id.equals(getAssetResponse.getId())) {
+				return getAssetResponse;
+			}
+		}
+		return null;
+	}
 
 //	@GetMapping
 //	public String sayHello(){
